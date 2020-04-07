91e2e9c574ceb4da668a18fe2e724c674e5efa79
John Young <john@codeandcreative.com>
2020-04-07 13:50:59 -0700

    Removes dependency on React Dev Tools

--- client/src/redux/middleware/index.js
+++ client/src/redux/middleware/index.js
@@ -4,7 +4,9 @@ import { logger, userEvent } from "./logger";
 
 // Redux Thunk middleware allows you to write action creators that return a function instead of an action
 
-export default compose(
-  applyMiddleware(thunk, logger, userEvent),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// React Dev Tools are not installed in all browsers. This checks for React Dev Tools.
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default composeEnhancers(
+  applyMiddleware(thunk, logger, userEvent)
 );
