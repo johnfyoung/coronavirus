37bb0b3ceecf7248efaa19d4472665dcd64ed09f
John Young <john@codeandcreative.com>
2020-06-27 11:24:18 -0700

    Adds memory usage measure to job

--- jobs/retrieveJohnsHopkins.js
+++ jobs/retrieveJohnsHopkins.js
@@ -4,6 +4,10 @@ import { dbg, logJob, logError } from "../util/tools";
 
 require("dotenv").config();
 mongoose.set("useUnifiedTopology", true);
+const used = process.memoryUsage();
+for (let key in used) {
+  console.log(`${key} ${Math.round((used[key] / 1024 / 1024) * 100) / 100} MB`);
+}
 
 mongoose
   .connect(process.env.MONGODB_URI, {
