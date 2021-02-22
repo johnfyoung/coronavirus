53f18a7926d01b4d6bac27af28f33b435eeec696
John Young <john@codeandcreative.com>
2021-02-21 22:01:39 -0800

    adds npx call to index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+const { exec } = require("child_process");
+
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from the lambda branch!"),
-  };
-  return response;
+  exec("npx babel-node jobs/retrieveJohnsHopkins.js", (err, stdout, stderr) => {
+    let response = {};
+
+    if (err) {
+      //some err occurred
+      response = {
+        statusCode: 503,
+        body: JSON.stringify({ err: `${err}` }),
+      };
+    } else {
+      // the *entire* stdout and stderr (buffered)
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({ stdout: `${stdout}`, stderr: `${stderr}` }),
+      };
+    }
+
+    return response;
+  });
 };
